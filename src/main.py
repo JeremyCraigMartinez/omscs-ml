4d7d875abe3fe4bcf2d0a3e0dd9c2a595e9ed7e0
JeremyCraigMartinez <jeremymartinez11@gmail.com>
2018-02-03 11:05:31 -0800

    optimize support vector machine algorithm

--- src/main.py
+++ src/main.py
@@ -21,6 +21,7 @@ rp_alg_arguments = {
         'input_dim': 1500,
         'units': 400,
     },
+    'support_vector_machine': {},
 }
 
 hp_alg_arguments = {
@@ -33,22 +34,27 @@ hp_alg_arguments = {
         'input_dim': 46,
         'units': 600,
     },
+    'support_vector_machine': {
+        'coef0': 1.25
+    }
 }
 def run_all(data, **kargs):
     split_data_set = data()
-    results = []
 
-    results.append(('boosting', boosting(*split_data_set)))
-    results.append(('k_nearest_neighbor', knn(*split_data_set)))
-    results.append(('decision_tree', decision_tree(*split_data_set, **kargs['decision_tree'])))
-    results.append(('neural_network', ann(*split_data_set, **kargs['neural_network'])))
-    results.append(('support_vector_machine', svm(*split_data_set)))
+    print('Confusion matrix for boosting')
+    boosting(*split_data_set)
 
-    for result in results:
-        alg = result[0]
-        cm = result[1]
+    print('Confusion matrix for k_nearest_neighbor')
+    knn(*split_data_set)
 
-        print('Confusion matrix for {}: {}'.format(alg, cm))
+    print('Confusion matrix for decision_tree')
+    decision_tree(*split_data_set, **kargs['decision_tree'])
+
+    print('Confusion matrix for neural_network')
+    ann(*split_data_set, **kargs['neural_network'])
+
+    print('Confusion matrix for support_vector_machine')
+    svm(*split_data_set, **kargs['support_vector_machine'])
 
 if __name__ == '__main__':
     # setting max depth for this dataset does not improve predicition accuracy
--- src/support_vector_machine.py
+++ src/support_vector_machine.py
@@ -2,18 +2,24 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
 from sklearn.metrics import confusion_matrix
 
-def svm(X_train, X_test, y_train, y_test):
-    # Feature Scaling
-    sc = StandardScaler()
-    X_train = sc.fit_transform(X_train)
-    X_test = sc.transform(X_test)
-
+def get_classifier(X_train, X_test, y_train, y_test, kernel, **kargs):
     # Fitting SVM to the Training set
-    classifier = SVC(kernel='linear', random_state=0)
+    classifier = SVC(kernel=kernel, **kargs)
     classifier.fit(X_train, y_train)
 
     # Predicting the Test set results
     y_pred = classifier.predict(X_test)
 
     # Making the Confusion Matrix
-    return confusion_matrix(y_test, y_pred)
+    print('svm with {} kernel\n'.format(kernel), confusion_matrix(y_test, y_pred))
+
+def svm(X_train, X_test, y_train, y_test, **kargs):
+    # Feature Scaling
+    sc = StandardScaler()
+    X_train = sc.fit_transform(X_train)
+    X_test = sc.transform(X_test)
+
+    get_classifier(X_train, X_test, y_train, y_test, 'linear', **kargs)
+    get_classifier(X_train, X_test, y_train, y_test, 'rbf', **kargs)
+    get_classifier(X_train, X_test, y_train, y_test, 'poly', **kargs)
+    get_classifier(X_train, X_test, y_train, y_test, 'sigmoid', **kargs)
