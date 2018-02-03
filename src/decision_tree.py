3b3d80608dc234df9cdb72e094cd6a7378ae32d4
JeremyCraigMartinez <jeremymartinez11@gmail.com>
2018-02-03 09:53:51 -0800

    optimize decision tree algorithm

--- src/decision_tree.py
+++ src/decision_tree.py
@@ -1,15 +1,35 @@
+# From scikit-learn docs:
+#     Decision-tree learners can create over-complex trees that do not
+#     generalise the data well. This is called overfitting. Mechanisms
+#     such as pruning (not currently supported), setting the minimum
+#     number of samples required at a leaf node or setting the maximum
+#     depth of the tree are necessary to avoid this problem.
+#
+#     Pruning done to avoid this: precondition approached such as setting
+#     max depth and weights to leaf nodes (where applicable)
+
 from sklearn.preprocessing import StandardScaler
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import confusion_matrix
 
-def decision_tree(X_train, X_test, y_train, y_test):
+def decision_tree(X_train,
+                  X_test,
+                  y_train,
+                  y_test,
+                  max_depth=None,
+                  random_state=None,
+                  min_weight_fraction_leaf=0.0):
     # Feature Scaling
     sc = StandardScaler()
     X_train = sc.fit_transform(X_train)
     X_test = sc.transform(X_test)
 
     # Fitting Decision Tree Classification to the Training set
-    classifier = DecisionTreeClassifier(criterion='entropy', random_state=0)
+    classifier = DecisionTreeClassifier(criterion='entropy',
+                                        random_state=random_state,
+                                        max_depth=max_depth,
+                                        min_weight_fraction_leaf=min_weight_fraction_leaf)
+
     classifier.fit(X_train, y_train)
 
     # Predicting the Test set results
--- src/main.py
+++ src/main.py
@@ -13,14 +13,33 @@ from support_vector_machine import svm
 
 warnings.filterwarnings('ignore')
 
+rp_alg_arguments = {
+    'decision_tree': {
+        'random_state': 100,
+    },
+    'neural_network': {
+        'input_dim': 1500
+    },
+}
+
+hp_alg_arguments = {
+    'decision_tree': {
+        'max_depth': 5,
+        'min_weight_fraction_leaf': 0.05,
+        'random_state': 100,
+    },
+    'neural_network': {
+        'input_dim': 46
+    },
+}
 def run_all(data, **kargs):
     split_data_set = data()
     results = []
 
     results.append(('boosting', boosting(*split_data_set)))
     results.append(('k_nearest_neighbor', knn(*split_data_set)))
-    results.append(('decision_tree', decision_tree(*split_data_set)))
-    results.append(('neural_network', ann(*split_data_set, **kargs)))
+    results.append(('decision_tree', decision_tree(*split_data_set, **kargs['decision_tree'])))
+    results.append(('neural_network', ann(*split_data_set, input_dim=kargs['input_dim'])))
     results.append(('support_vector_machine', svm(*split_data_set)))
 
     for result in results:
@@ -30,5 +49,6 @@ def run_all(data, **kargs):
         print('Confusion matrix for {}: {}'.format(alg, cm))
 
 if __name__ == '__main__':
-    run_all(rp, input_dim=1500)
-    run_all(hp, input_dim=46)
+    # setting max depth for this dataset does not improve predicition accuracy
+    run_all(rp, **rp_alg_arguments)
+    run_all(hp, **hp_alg_arguments)
