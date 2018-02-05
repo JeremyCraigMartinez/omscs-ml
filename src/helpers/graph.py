09717ffa8a6a28e6eae5b50de5a4cf2dda223070
JeremyCraigMartinez <jeremymartinez11@gmail.com>
2018-02-04 20:34:33 -0800

    add finished analysis

--- src/helpers/graph.py
+++ src/helpers/graph.py
@@ -9,10 +9,12 @@ def plot_line(mplot, classifier, label, X, y, cv=None,
                                      cv=cv, n_jobs=n_jobs,
                                      train_sizes=train_sizes,
                                      scoring=scoring)
-    train_size, train_scores, _ = learn_curve_res
+    train_size, train_scores, test_scores = learn_curve_res
 
     train_scores_mean = np.mean(train_scores, axis=1)
     train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
     mplot.grid()
 
     mplot.fill_between(train_size,
@@ -20,18 +22,24 @@ def plot_line(mplot, classifier, label, X, y, cv=None,
                        1 - train_scores_mean - train_scores_std,
                        alpha=0.1, color=color)
     mplot.plot(train_size, 1 - train_scores_mean, 'o-',
-               color=color, label=label)
+               color=color[0], label=label)
 
+    plt.fill_between(train_size,
+                     1 - test_scores_mean + test_scores_std,
+                     1 - test_scores_mean - test_scores_std,
+                     alpha=0.1, color=color)
+    plt.plot(train_size, 1 - test_scores_mean, 'o-',
+             color=color[1], label='Test score')
 
-def plot(line, X, y, cv=None):
+def plot(line, X, y, title='', cv=None):
     plt.figure()
-    plt.title('All algorithms')
+    plt.title(title)
     plt.xlabel("Training examples")
-    plt.ylabel("Cost = - 1 Score")
+    plt.ylabel("Error Rate")
 
-    colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k']
+    colors = ['b', 'g', 'r', 'c', 'm', 'y']
     for (classifier, label) in line:
-        plot_line(plt, classifier, label, X, y, cv=cv, color=colors.pop())
+        plot_line(plt, classifier, label, X, y, cv=cv, color=[colors.pop(), colors.pop()])
 
     plt.legend(loc='best')
     plt.show()
