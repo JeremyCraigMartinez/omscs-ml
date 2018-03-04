b3c4cdaa3b72a572f0e35601b6b0670cac692365
JeremyCraigMartinez <jeremymartinez11@gmail.com>
2018-03-03 17:52:14 -0800

    initial commit with Assignment 2 code
    cloned ABIGAIL into repo (rather than forking repo)

---
+++ Assignment-2/src/helpers/reviews_preprocessing.py
@@ -0,0 +1,44 @@
+from os.path import dirname, realpath
+import re
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+dir_path = dirname(realpath(__file__))
+
+import nltk
+# uncomment if you do not have stopwords downloaded
+#nltk.download('stopwords')
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+def get_corpus(dataset):
+    corpus = []
+    for i in range(0, 1000):
+        review = re.sub('[^a-zA-Z]', ' ', dataset['Review'][i])
+        review = review.lower()
+        review = review.split()
+        ps = PorterStemmer()
+        review = [ps.stem(word) for word in review if not word in set(stopwords.words('english'))]
+        review = ' '.join(review)
+        corpus.append(review)
+    return corpus
+
+# Creating the Bag of Words model
+from sklearn.feature_extraction.text import CountVectorizer
+def get_X_Y(corpus, dataset):
+    cv = CountVectorizer(max_features=1500)
+    X = cv.fit_transform(corpus).toarray()
+    y = dataset.iloc[:, 1].values
+
+    return X, y
+
+# Splitting the dataset into the Training set and Test set
+def get_train_test_set():
+    # Importing the dataset
+    dataset = pd.read_csv(dir_path + '/../../data/Restaurant_Reviews.tsv', delimiter='\t', quoting=3)
+
+    corpus = get_corpus(dataset)
+    X, y = get_X_Y(corpus, dataset)
+
+    # X_train, X_test, y_train, y_test
+    return train_test_split(X, y, test_size=0.2)
---
+++ Assignment-2/src/main.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+
+from helpers.reviews_preprocessing import get_train_test_set as rp
+
+from func.nn.backprop import BackPropagationNetworkFactory
+from shared import SumOfSquaresError, DataSet, Instance
+from opt.example import NeuralNetworkOptimizationProblem
+from func.nn.backprop import RPROPUpdateRule, BatchBackPropagationTrainer
+import opt.RandomizedHillClimbing as RandomizedHillClimbing
+import opt.SimulatedAnnealing as SimulatedAnnealing
+import opt.ga.StandardGeneticAlgorithm as StandardGeneticAlgorithm
+from func.nn.activation import RELU
+
+class RandomSearch(object):
+  """docstring for RandomSearch"""
+  def __init__(self, **kargs):
+    super(RandomSearch, self).__init__()
+    self.outfile = kargs['outfile']
+    self.search_alg = kargs['search_alg']
+
+  def __str__(self):
+    return self.outfile
+
+  def read_data(self):
+    X_train, X_test, y_train, y_test = rp()
+    self.X_train = X_train
+    self.X_test = X_test
+    self.y_train = y_train
+    self.y_test = y_test
+
+  def get_error(self, data):
+
+
+if __name__ == '__main__':
+  rhc = RandomSearch(**{'outfile': 'RHC'})
+  print(rhc)
